9db2c55ef52cf640aa19894b22530eecc5a86d28
Leon Si <leonzalion@gmail.com>
2020-05-10 02:40:44 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+const express = require('express');
+const port = process.env.PORT || 3000;
+const app = express();
+const mongoose = require('mongoose');
+const to = require('await-to-js').default;
+
+mongoose.connect(`mongodb+srv://paranoia:${process.env.DB_PASS}@userinexperience-maztg.mongodb.net/test`);
+
+const User = require('models/User');
+
+app.get('/users', async (req, res) => {
+  const [error, users] = await to(User.find({}).exec());
+  if (error) return res.json({success: false, error});
+  return res.json({success: true, data: users});
+});
+
+app.post('/users', async (req, res) => {
+  const { username, time, avatar } = req.body;
+  const [error] = await to(User.create({username, time, avatar}));
+  if (error) return res.json({success: false, error});
+  return res.json({success: true});
+});
+
+app.listen(port, () => {
+  console.log('Example app listening on port 3000!');
+});
